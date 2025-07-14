72894c446ffa5a63094a8d38510070e8975992e0
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-07-14 13:00:40 -0300

    Add files via upload

--- app/list.jsx
+++ app/list.jsx
@@ -4,8 +4,8 @@ import mock from "../constants/mock"
 
 export default function List(){
     return(
-        <SafeAreaView>
-            <ScrollView>
+        <SafeAreaView style={s.wrap}>
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
             <Text>Lista de cards</Text>
             <FlatList data={mock} keyExtractor={(item) => item.id} renderItem={({item}) => <Card id={item.id} name={item.nome} age={item.idade} date={item.data} img={item.imagem}/>}/>
             </ScrollView>
@@ -14,5 +14,7 @@ export default function List(){
 }
 
 const s = StyleSheet.create({
-
+  wrap: {
+    flex: 1,
+  },
 })
